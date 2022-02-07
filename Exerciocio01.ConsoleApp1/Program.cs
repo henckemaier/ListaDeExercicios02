29150287f866e8c5c784e604dc7b49229da5f59a
eduhe <eduhe@DESKTOP-HVUF937>
2022-02-07 16:36:25 -0300

    Add project files.

---
+++ Exerciocio01.ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerciocio01.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
